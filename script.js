8637f4295e56855b0bd06f5b793742d89c72692b
FelipeP <pontesfelipe@outlook.com>
2022-01-17 22:33:49 -0300

    Criação de calculadora funcional com somente o uso do JavaScript

---
+++ script.js
@@ -0,0 +1,75 @@
+//Projeto Calculadora JS
+
+/*Tipos primitivos
+
+//Boleandos
+var vOuf = false;
+console.log (typeof(vOuf));
+
+//number
+var number = 1;
+console.log(typeof(number));
+
+//string
+var nome = 'Felipe';
+console.log(typeof(nome));
+
+//adição
+var adicao = 1+1;
+console.log(adicao);
+
+//subtração
+var subtracao = 2-1;
+console.log(subtracao);
+
+//multiplicação
+var multiplic = 2*3;
+console.log(multiplic);
+
+//divisão real
+var divReal = 6/2;
+console.log(divReal);
+
+//divisão inteira
+var divInteira = 5%6;
+console.log(divInteira);
+
+//Potenciação
+var pot = 2**1;
+console.log(pot);
+
+//vetroes
+let array = ['string', 1, true];
+console.log(array); 
+
+// objetos
+let object = {String: 'string', number: 1, boolean: true, array:['array']};
+console.log(object.array); 
+
+var jogador1 = 1;
+var jogador2 = 0;
+var placar;
+
+if (jogador1 > 0 && jogador2 == 0) {
+    console.log('jogador 1 marcou ponto!');
+}
+
+
+function funcao() {
+    console.log('Ok');
+}
+funcao(); 
+
+//declarativa
+function funcao() {
+    console.log('opa')
+}
+funcao () 
+
+var funcao = () => {
+    console.log('sou uma arrow')
+
+}
+funcao() */
+
+
